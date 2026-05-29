5b847ea8bf745c5b9e0584cf2a4107d7482afef6
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-05-29 06:15:16 +0530

    feat: add MongoRelaySource struct with builder methods

---
+++ source/mongo/source/source.go
@@ -0,0 +1,40 @@
+package source
+
+import (
+	"github.com/Srajan-Sanjay-Saxena/cdcrelay/engine/engine_source"
+	"github.com/Srajan-Sanjay-Saxena/cdcrelay/source/mongo/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"fmt"
+)
+
+const resumeTokenKey = "mongo:resume_token"
+
+type MongoRelaySource struct {
+	cfg        *config.MongoRelaySourceConfig
+	mongoClient     *mongo.Client
+	collection *mongo.Collection
+	store      engine_source.PersistenceStore
+	producer   engine_source.Producer
+	cs         *mongo.ChangeStream
+}
+
+func NewSource(cfg *config.MongoRelaySourceConfig) *MongoRelaySource {
+	return &MongoRelaySource{cfg: cfg}
+}
+
+func (s *MongoRelaySource) AddPersistanceStore(store engine_source.PersistenceStore) *MongoRelaySource {
+	s.store = store
+	return s
+}
+
+func (s *MongoRelaySource) AddProducer(producer engine_source.Producer) *MongoRelaySource {
+	s.producer = producer
+	return s
+}
+
+func (s *MongoRelaySource) GetProducer() (engine_source.Producer, error) {
+	if s.producer == nil {
+		return nil, fmt.Errorf("producer not initialized")
+	}
+	return s.producer, nil
+}
