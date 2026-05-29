aed80c51cb37ee31fdb449e768e4bc2c5c11d37e
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-05-29 06:14:58 +0530

    feat: add EngineSource, Producer, PersistenceStore contracts

---
+++ engine/engine_source/contract.go
@@ -0,0 +1,30 @@
+package engine_source
+
+import (
+	"context"
+	"github.com/Srajan-Sanjay-Saxena/cdcrelay/engine/event"
+)
+
+type EngineSource interface {
+	DBConnect(ctx context.Context) error
+	CaptureEvents(ctx context.Context) (<-chan event.Event, error)
+	Ack(ctx context.Context) error
+	Close(ctx context.Context) error
+	GetProducer() (Producer, error)
+}
+
+type Producer interface {
+	Connect(ctx context.Context) error
+	Publish(ctx context.Context, event event.Event) error
+	Close() error
+}
+
+
+type PersistenceStore interface {
+	Connect(ctx context.Context) error
+	CloseStoreConnection(ctx context.Context) error
+	Save(ctx context.Context, key string, value []byte) error
+	Load(ctx context.Context, key string) ([]byte, error)
+	Delete(ctx context.Context, key string) error
+}
+
